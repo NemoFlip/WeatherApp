47ccfaf99ca1b0e95df7d1d7e59674bce01e07ef
NemoFlip <artgamer3163@gmail.com>
2022-05-01 14:41:02 +0300

    Completed hourly forecast

--- Weather/Views/SubViews/HourlyForecastRow.swift
+++ Weather/Views/SubViews/HourlyForecastRow.swift
@@ -11,7 +11,8 @@ struct HourlyForecastRow: View {
     let hourlyModel: Current
     let timeOffset: Int
     var body: some View {
-        VStack(spacing: 10) {
+//        VStack(spacing: (hourlyModel.pop ?? 0) >= 0.1 ? 2 : 10) {
+        VStack {
             Group {
                 Text("\(checkNowTime() ? "Now" : getDateFromUNIX(timeOffset: timeOffset, currentDate: hourlyModel.dt ,dateFormat: "h"))")
                     .fontWeight(.medium)
@@ -19,22 +20,24 @@ struct HourlyForecastRow: View {
                 Text("\(checkNowTime() ? "" : getDateFromUNIX(timeOffset: timeOffset, currentDate: hourlyModel.dt ,dateFormat: "a"))")
                     .fontWeight(.medium)
                     .font(.footnote)
-            }
+            }.frame(maxHeight: .infinity, alignment: .top)
             VStack(spacing: 0) {
                 Image(systemName: hourlyModel.weather[0].getIconName().name)
                     .foregroundStyle(hourlyModel.weather[0].getIconName().primaryColor, hourlyModel.weather[0].getIconName().secondaryColor, Color.theme.lightBlue)
-                    .font(.title3)
+                    .font((hourlyModel.pop ?? 0) >= 0.1 ? .title3 : .title2)
                 if let rainChance = hourlyModel.pop, rainChance >= 0.1 {
                     Text("\(Int(rainChance * 100))%")
                         .foregroundColor(.theme.lightBlue)
                         .font(.system(size: 14))
                 }
             }
+            .frame(maxHeight: .infinity, alignment: .center)
             Text("\(Int(round(hourlyModel.temp)))º")
+                .frame(maxHeight: .infinity, alignment: .bottom)
         }
     }
     func checkNowTime() -> Bool {
-        getDateFromUNIX(timeOffset: timeOffset, currentDate: hourlyModel.dt ,dateFormat: "h") == getDateFromUNIX(timeOffset: timeOffset, currentDate: Int(Date().timeIntervalSince1970), dateFormat: "h")
+        getDateFromUNIX(timeOffset: timeOffset, currentDate: hourlyModel.dt ,dateFormat: "d, h") == getDateFromUNIX(timeOffset: timeOffset, currentDate: Int(Date().timeIntervalSince1970), dateFormat: "d, h")
     }
 }
 
